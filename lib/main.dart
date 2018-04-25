13769f1f700f106fcb8b4740fd5043548279fd13
Adil <chmuhammadadil7@gmail.com>
2018-04-25 16:25:32 +0500

    added navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/foundation.dart';
 import 'package:hello_flutter/screens/chat_screen/ChatScreen.dart';
+import 'package:hello_flutter/screens/splash_screen/SplashScreen.dart';
 
 void main() => runApp(new MyApp());
 
@@ -13,7 +14,7 @@ class MyApp extends StatelessWidget {
       theme: defaultTargetPlatform == TargetPlatform.iOS
           ? kIOSTheme
           : kDefaultTheme,
-      home: new HomeScreen(),
+      home: new SplashScreen(),
     );
   }
 }
